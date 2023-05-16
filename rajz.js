bc4d96d3a527699a45cfe92f0a0040b57b699d25
Rajz Verma <132135229+rajz5044@users.noreply.github.com>
2023-05-17 00:45:49 +0545

    aa
    aa

---
+++ rajz.js
@@ -0,0 +1,18 @@
+function validate()
+{
+   var eid= document.getElementById("eid").value;
+   var pw= document.getElementById("pw").value;
+   if(eid=="rajzverma" && pw=="69")
+   {
+    window.open("pagal.html")
+   }
+   else{
+    alert("login failed.");
+   }
+    
+
+
+
+}
+
+
